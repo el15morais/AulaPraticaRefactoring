e5d9e5786db8e005926b560e42e95bd667f23244
Gabriel Morais <gabrielmorais290921@gmail.com>
2025-11-19 20:41:06 -0300

    Commit 4
    Commit 4

--- src/main/java/videolocadora/Rental.java
+++ src/main/java/videolocadora/Rental.java
@@ -10,6 +10,61 @@ public class Rental {
         this.daysRented = daysRented;
     }
 
+    private double amountFor(Rental aRental) {
+
+        double thisAmount = 0;
+
+        switch (aRental.getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (aRental.getDaysRented() > 2) {
+                    thisAmount += (aRental.getDaysRented() - 2) * 1.5;
+                }
+                break;
+
+            case Movie.NEW_RELEASE:
+                thisAmount += aRental.getDaysRented() * 3;
+                break;
+
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if (aRental.getDaysRented() > 3) {
+                    thisAmount += (aRental.getDaysRented() - 3) * 1.5;
+                }
+                break;
+        }
+
+        return thisAmount;
+    }
+
+    public double getCharge() {
+
+        double thisAmount = 0;
+
+        switch (movie.getPriceCode()) {
+
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (daysRented > 2) {
+                    thisAmount += (daysRented - 2) * 1.5;
+                }
+                break;
+
+            case Movie.NEW_RELEASE:
+                thisAmount += daysRented * 3;
+                break;
+
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if (daysRented > 3) {
+                    thisAmount += (daysRented - 3) * 1.5;
+                }
+                break;
+        }
+
+        return thisAmount;
+    }
+
     public Movie getMovie() {
         return movie;
     }
