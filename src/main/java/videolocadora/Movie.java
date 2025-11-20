080989b819752075fb6c10c84b832c4aed9e435e
Gabriel Morais <gabrielmorais290921@gmail.com>
2025-11-19 21:07:01 -0300

    Commit 14
    Commit 14

--- src/main/java/videolocadora/Movie.java
+++ src/main/java/videolocadora/Movie.java
@@ -43,11 +43,6 @@ public class Movie {
     }
 
     public int getFrequentRenterPoints(int daysRented) {
-        if (getPriceCode() == NEW_RELEASE && daysRented > 1) {
-            return 2;
-        }
-        return 1;
+        return price.getFrequentRenterPoints(daysRented);
     }
-
-
 }
